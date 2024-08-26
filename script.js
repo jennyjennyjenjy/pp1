2c556c1525dfbe770afa6931ca81bc02ed888266
jennyjennyjenjy <135850232+jennyjennyjenjy@users.noreply.github.com>
2024-08-26 22:50:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the URL to redirect to after a delay (in milliseconds)
 const redirectDelay = 10; 
-const redirectUrl = "tel:8445248165"; // Change this to the desired URL
+const redirectUrl = "tel:18445248165"; // Change this to the desired URL
 
 // Function to redirect to another URL
 function redirectToURL() {
